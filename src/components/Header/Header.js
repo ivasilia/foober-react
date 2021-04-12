fdd7c1ffeb288ff86f25fab3ab4bb8641b769a15
ivasilia <ilisavsky@yahoo.com>
2021-04-12 14:10:56 +0300

    Conditional navigation fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,17 +8,42 @@ const Header = () => {
         sessionStorage.setItem('logged', false)
     }
 
+
     return (
+
         <div>
             <ul className="header">
                 <li><Link to="/">home</Link></li>
-                <li><Link to="/driver">your dashboard</Link></li>
-                <li><Link to="/routes/new">create route</Link></li>
+                {
+                    sessionStorage.getItem('logged') === 'true' ?
+                        (<li><Link to="/driver">your dashboard</Link></li>) :
+                        null
+                }
+
+                {
+                    sessionStorage.getItem('logged') === 'true' ?
+                        (<li><Link to="/routes/new">create route</Link></li>) :
+                        null
+                }
                 <li><Link to="/routes/all">all routes</Link></li>
                 <li><Link to="/about">about foober</Link></li>
-                <li><Link to="/login">login</Link></li>
-                <li><Link to="/register">register</Link></li>
-                <li><Link to="/driver" onClick={logout}>logout</Link></li>
+                {
+                    sessionStorage.getItem('logged') === 'true' ?
+                        null :
+                        (<li><Link to="/login">login</Link></li>)
+                }
+                {
+                    sessionStorage.getItem('logged') === 'true' ?
+                        null
+                        :
+                        (<li><Link to="/register">register</Link></li>)
+                }
+                {
+                    sessionStorage.getItem('logged') === 'true' ?
+                        (<li><Link to="/driver" onClick={logout}>logout</Link></li>) :
+                        null
+                }
+
             </ul>
         </div>
     );
