fc01fa0234fbf0e628acb8b9ea526e5dd57f7e31
ivasilia <ilisavsky@yahoo.com>
2021-04-16 11:27:58 +0300

    Page re-constructed

--- src/components/Passenger/Passenger.js
+++ src/components/Passenger/Passenger.js
@@ -1,21 +1,41 @@
 import './Passenger.css';
 import LoginForm from '../Forms/LoginForm';
+import AuthContext from '../context/AuthContext';
 import PassengerDashboard from './PassengerDashboard';
-import RegPassengerForm from '../Forms/RegPassengerForm';
 import { Link } from 'react-router-dom';
+import { useEffect, useContext, useState } from 'react';
 
 const Passenger = ({
     match
 }) => {
 
-    if (sessionStorage.getItem('logged') == 'true') {    // ---- 'true' as string!!! ----
+    const { user, setUser } = useState({});
+    const context = useContext(AuthContext);
+
+
+    if (sessionStorage.getItem('logged') === 'true' && sessionStorage.getItem('userType') === 'passenger') {    // ---- 'true' as string!!! ----
         return <PassengerDashboard userId={match.params.id} />;
 
     } else {
         return (
             <>
-            <LoginForm />
-            <Link to="/passengers/register" ><h4>Have no account yet! Sign up!</h4></Link>
+                <p>Passenger login</p>
+                <div>
+                    <form onSubmit={context.onSubmitHandler} id="passenger">
+                        <div className="inputField">
+                            <label htmlFor="username">Enter username</label>
+                            <input type="text" id="username" name="username" />
+                        </div>
+                        <div className="inputField">
+                            <label htmlFor="password">Enter password</label>
+                            <input type="password" id="password" name="password" />
+                        </div>
+                        <div className="inputField">
+                            <input type="submit" value="Submit" />
+                        </div>
+                    </form>
+                </div>
+                <Link to="/passengers/register" ><h4>Have no account yet! Sign up!</h4></Link>
             </>
         );
     }
