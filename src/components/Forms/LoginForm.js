76f400ed72ed6be11f272a9b98210e376f9f8f4a
ivasilia <ilisavsky@yahoo.com>
2021-04-16 11:29:35 +0300

    Added userType

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -1,14 +1,14 @@
 import './Form.css';
 import AuthContext from '../context/AuthContext';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
-const LoginForm = () => {
+const LoginForm = (props) => {
 
     const context = useContext(AuthContext);
 
     return (
         <div>
-            <form onSubmit={context.onSubmitHandler}>
+            <form onSubmit={context.onSubmitHandler} id={props.userType}>
                 <div className="inputField">
                     <label htmlFor="username">Enter username</label>
                     <input type="text" id="username" name="username" />
