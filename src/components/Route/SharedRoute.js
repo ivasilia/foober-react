51da980b52274dc25bed963c5babdd7bb6d4ec34
ivasilia <ilisavsky@yahoo.com>
2021-04-16 11:26:13 +0300

    Fixed fetch

--- src/components/Route/SharedRoute.js
+++ src/components/Route/SharedRoute.js
@@ -1,34 +1,49 @@
 import { host } from '../../common/constants';
-import { useEffect, useState } from 'react';
+import { Component } from 'react';
 import './Route.css';
 
-const SharedRoute = (props) => {
-    console.log(props.location.origcity);
+class SharedRoute extends Component {
 
-    const { data, setData } = useState([]);
-
-    // ------------- TODO fix returning route ID (request successful, entity being created) ---------
-    useEffect(async () => {
-        fetch(`${host}/routes/${props.location.id}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(res => res.json())
-        .then(data => console.log(data));
-    }, []);
-
-
-return (
-    <div className="new-shared-route">
-        <p><b>Shared Route Id nr: ----</b></p>
-        <p>{props.location.origcity} - {props.location.destcity}</p>
-        <p>{props.location.distance} km</p>
-    </div>
-);
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            origin: '',
+            destination: '',
+            distance: 0
+        }
     }
+
+
+    componentDidMount = async () => {
+        let routeId = this.props.match.params.id;
+        console.log(`Use effect fired with: ${routeId}`);
+
+        await fetch(`${host}/routes/${routeId}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(`Fetched route: ${data.id} ${data.origin}`);
+                this.setState({ id: data.id });
+                this.setState({ origin: data.origin });
+                this.setState({ destination: data.destination });
+                this.setState({ distance: data.distance });
+            })
+            .catch(err => console.log(err));
+    }
+
+
+    render() {
+        return (
+            <div className="new-shared-route" >
+                <p><b>Shared Route Id nr: {this.state.id}</b></p>
+                <p>{this.state.origin} - {this.state.destination}</p>
+                <p>{this.state.distance} km</p>
+                <button >Enter route</button>
+            </div>
+        );
+    }
+
+}
 
 
 export default SharedRoute;
