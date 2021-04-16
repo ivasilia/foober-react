dfda8b5ba01e9167924c7f7c022900cc2af92977
ivasilia <ilisavsky@yahoo.com>
2021-04-16 11:28:29 +0300

    re-constructed

--- src/components/Init/Landing.js
+++ src/components/Init/Landing.js
@@ -1,12 +1,22 @@
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Init from './Init';
 
 const Landing = () => {
+
+    useEffect(async () => {
+        await sessionStorage.setItem('userType', '');
+    }, []);
+
     return (
         <div>
-            <Link to="/drivers"><Init text="DRIVER" /></Link>
+            <Link to="/drivers"
+            >
+                <Init text="DRIVER" /></Link>
             <Link to="/about"><p>Welcome to Foober. Read more...</p></Link>
-            <Link to="/passengers"><Init text="PASSENGER" /></Link>
+            <Link to="/passengers"
+            >
+                <Init text="PASSENGER" /></Link>
         </div>
     )
 }
