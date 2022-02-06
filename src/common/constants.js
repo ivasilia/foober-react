c1b4ed8835fb7160ae44a92cb73ae72bc2d8023f
ivasilia <54475918+ivasilia@users.noreply.github.com>
2022-02-06 16:06:46 +0200

    Update constants.js

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,6 +1,6 @@
 const host = 
-'http://localhost:8080';
-// 'https://foober-java.herokuapp.com';
+// 'http://localhost:8080';
+'https://foober-java.herokuapp.com';
 const googleApiKey = 'AIzaSyDHDbORUyOSxRBZj9flx736bV7mrqglhBM';
 const googleDistanceMatrixUrl = 'http://localhost:8080/routes/distance?';
 const townsReqUrl = 'https://simplemaps.com/static/data/country-cities/at/at.json';
